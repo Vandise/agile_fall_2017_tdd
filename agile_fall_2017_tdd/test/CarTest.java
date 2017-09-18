ece9862628af291be6bd6faee6ebc6bb593efdea
leducrd <leducrd@gmail.com>
2017-09-17 20:40:10 -0500

    Add test case cannotDecelerateBelowZero()

--- agile_fall_2017_tdd/test/CarTest.java
+++ agile_fall_2017_tdd/test/CarTest.java
@@ -41,5 +41,12 @@ public class CarTest {
 	}
 	
 	// Create and pass test for no deceleration below 0.
+	@Test
+	public void cannotDecelerateBelowZero() {
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(10);
+		car1.decelerate(20);
+		assertThat(car1.getSpeed(), is(0));
+	}
 	
 }
