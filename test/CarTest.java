d23540d35dee275ca46eff1be9e576d6e714c0cb
wrongholt <wrongholt@gmail.com>
2017-09-19 21:24:15 -0500

    Implemented decelorate test

--- test/CarTest.java
+++ test/CarTest.java
@@ -32,7 +32,13 @@ public class CarTest {
 	}
 
 	// Create and pass test for deceleration.
-	
+	@Test
+	public void decelerate(){
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(20);
+		car1.decelerate(10);
+		assertThat(car1.getSpeed(), is(10));
+	}
 	// Create and pass test for no deceleration below 0.
 	
 }
