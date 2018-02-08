e15b2b3332bb44d20ee7c5eb3b4103476346a26f
derikBeardshear <dbeardshear@student.cvtc.edu>
2018-02-08 14:56:10 -0600

    Fail decelertaion test.

--- test/CarTest.java
+++ test/CarTest.java
@@ -32,7 +32,13 @@ public class CarTest {
 	}
 
 	// Create and pass test for deceleration.
-	
+	@Test
+	public void canDecelerate(){
+		
+		Car car1 = new Car("Red", 100);
+		assertThat(car1.getSpeed(), is(95));
+		
+	}
 	// Create and pass test for no deceleration below 0.
 	
 }
