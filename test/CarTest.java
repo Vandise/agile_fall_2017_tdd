abc02959a1c97ab5eea811bad322a2d5490e0d4b
Seth Schalinske <sethschalinske@SethsMac.local>
2018-02-08 11:55:53 -0600

    Wrote unit test to make sure the car cannot decelerate below zero

--- test/CarTest.java
+++ test/CarTest.java
@@ -40,5 +40,11 @@ public class CarTest {
 	}
 	
 	// Create and pass test for no deceleration below 0.
+	@Test
+	public void cannotDecelerateBelowZero() {
+		Car car1 = new Car("Red", 100);
+		car1.decelerate(10);
+		assertThat(car1.getSpeed(), is(0));
+	}
 	
 }
