d520003ec5a7230567ac413e1c269a9fa6752356
derikBeardshear <dbeardshear@student.cvtc.edu>
2018-02-08 15:07:01 -0600

    Fail decelerate past zero

--- test/CarTest.java
+++ test/CarTest.java
@@ -42,5 +42,12 @@ public class CarTest {
 		
 	}
 	// Create and pass test for no deceleration below 0.
+	@Test
+	public void cannotDeceleratePastZero(){
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(10);
+		car1.decelerate(11);
+		assertThat(car1.getSpeed(), is(0));
+	}
 	
 }
