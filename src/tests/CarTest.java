b85b87d4f804fbf951eb59ad8610990e85b80228
Benjamin Miles <dootbin@dootbin>
2018-02-12 16:48:17 -0600

    Added decelerate test

--- src/tests/CarTest.java
+++ src/tests/CarTest.java
@@ -11,9 +11,18 @@ import tests.CarTest;
 
 public class CarTest {
 
-	@Test
-public	void testCarToString() {
+@Test
+public void testCarToString() {
 	assertThat(new Car("red", 100).toString(), is("Color: red, Speed: 0"));
+	}
+
+
+
+@Test
+public void testCarDecelerate() {
+	Car car = new Car("red", 100);
+	car.accelerate(20);
+	assertThat(car.getSpeed(), is(10));
 	}
 
 }
