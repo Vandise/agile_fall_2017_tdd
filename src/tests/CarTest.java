907d7360e9581b40e769f18bfc03e19360ce3458
Benjamin Miles <dootbin@dootbin>
2018-02-12 09:18:08 -0600

    added to.string() unit test

--- src/tests/CarTest.java
+++ src/tests/CarTest.java
@@ -1,14 +1,18 @@
 package tests;
-
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Test;
+
+import main.Car;
+import tests.CarTest;
 
 public class CarTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	void testCarToString() {
+		assertThat(new Car("red", 100).toString(), is("Color: red, Speed: 100"));
 	}
 
 }
