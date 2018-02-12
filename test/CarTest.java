727bcbd1549842b07105ed035e346f4e88bba1bb
Pete <prasmusse@student.cvtc.edu>
2018-02-11 23:08:21 -0600

    implemented CannotDecelerateBelowZeroTest

--- test/CarTest.java
+++ test/CarTest.java
@@ -41,5 +41,11 @@ public class CarTest {
   }
 	
 	// Create and pass test for no deceleration below 0.
-	
+	@Test 
+  public void cannotDecelerateBelowZero(){
+    Car car1 = new Car("Red", 100);
+    car1.accelerate(10);
+    car1.decelerate(11);
+    assertThat(car1.getSpeed(), is(0));
+  }
 }
