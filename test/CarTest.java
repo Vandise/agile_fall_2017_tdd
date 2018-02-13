d03e34ada82840ef12b306507060b90a05c272f6
Sam Peterson <speterson96@student.cvtc.edu>
2018-02-13 13:34:45 -0600

    cleaned up code

--- test/CarTest.java
+++ test/CarTest.java
@@ -7,47 +7,54 @@ public class CarTest {
 
 	@Test 
 	public void createsCarWithSpeedOfZero() {
+		
 		Car car1 = new Car("Red", 100);
 		assertThat(car1.getSpeed(), is(0));
+		
 	}
 	
 	@Test 
 	public void canAccelerate(){
+		
 		Car car1 = new Car("Red", 100);
 		car1.accelerate(10);
 		assertThat(car1.getSpeed(), is(10));
+		
 	}
 	
 	@Test
 	public void cannotAccelerateBeyondMaxSpeed(){
+		
 		Car car1 = new Car("Red", 100);
 		car1.accelerate(101);
 		assertThat(car1.getSpeed(), is(100));
+		
 	}
 	
 	@Test 
 	public void canDecelerate(){
+		
 		Car car1 = new Car("Red", 100);
 		car1.decelerate(-10);
 		assertThat(car1.getSpeed(), is(10));
+		
 	}
 	
 	@Test
 	public void cannotDecelerateBeyondMaxSpeed(){
+		
 		Car car1 = new Car("Red", 100);
 		car1.decelerate(0);
 		assertThat(car1.getSpeed(), is(0));
+		
 	}
 	
 	
 	@Test 
 	public void canDoToString(){
+		
 		Car car1 = new Car("Red", 100);
 		assertThat(car1.toString(), is("Color: Red, Speed: 0"));
+		
 	}
-
-	// Create and pass test for deceleration.
-	
-	// Create and pass test for no deceleration below 0.
-	
 }
