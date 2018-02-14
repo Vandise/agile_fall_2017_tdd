370e034551a64d6925ad96d4b7e7a90fde3cf9ef
William Ramirez <wramirezramos@student.cvtc.edu>
2018-02-13 23:11:03 -0600

    Implemented decelerate method for the car class

--- test/CarTest.java
+++ test/CarTest.java
@@ -36,10 +36,16 @@ public class CarTest {
 	public void canDecelerate(){
 		Car car1 = new Car("Red", 100);
 		car1.accelerate(70);
-		car1.decelerate(70);
-		assertThat(car1.getSpeed(), is(0));
+		car1.decelerate(65);
+		assertThat(car1.getSpeed(), is(30));
 	}
 	
 	// Create and pass test for no deceleration below 0.
-	
+	@Test
+	public void canDecelerateMoreThanZero(){
+		Car car1 = new Car("Red", 100);
+		car1.accelerate(100);
+		car1.decelerate(70);
+		assertThat(car1.getSpeed(), is(0));
+	}
 }
