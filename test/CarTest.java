93badb22019e20e8415ddcfdd278ca8e7ddef08e
Benjamin J. Anderson <andeb2804@gmail.com>
2018-02-21 16:40:17 -0600

    add test for decelerate method

--- test/CarTest.java
+++ test/CarTest.java
@@ -31,6 +31,15 @@ public class CarTest {
 		assertThat(car1.toString(), is("Color: Red, Speed: 0"));
 	}
 
+
+  @Test
+  public void canDecelerate()
+  {
+    Car car1 = new Car("Red", 100);
+    car1.decelerate(50);
+    assertThat(car.getSpeed(), is(-50));
+  }
+
 	// Create and pass test for deceleration.
 	
 	// Create and pass test for no deceleration below 0.
